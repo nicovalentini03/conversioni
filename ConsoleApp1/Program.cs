810a1638f34c9a9daf297aa568aee68109ac3769
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:43:22 +0200

    conversione di un numero da decimale a esadecimale e da decimale a ottale

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,11 @@ namespace ConsoleApp1
             Console.WriteLine("inserisci un numero da convertire");
             int value =int.Parse(Console.ReadLine());
             string binary = Convert.ToString(value, 2);
-            Console.WriteLine($"{binary}");
+            Console.WriteLine($"il corrispettivo numero binario è {binary}");
+            string esadecimale = Convert.ToString(value, 16);
+            Console.WriteLine($"il corrispettivo numero esadecimale è {esadecimale}");
+            string ottale = Convert.ToString(value, 8);
+            Console.WriteLine($"il corrispettivo numero ottale è {ottale}");
             Console.ReadLine();
 
         }
